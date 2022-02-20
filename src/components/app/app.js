5554766541914e3c119bd042fe6dce7aa58b4261
Dmitriy <fadeev97-1997@yandex.ru>
2022-02-20 17:21:02 +0400

    start

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './app.module.scss'
+
+export default function App() {
+  return (
+      <div className={styles.app}>
+        app will be started soon
+      </div>
+  );
+}
